2305baa590aff7c4db34ba7d14ab60d3951d658a
Subash Gauatm <subashgautam977@gmail.com>
2025-02-09 00:15:21 +0545

    sending private message to a member : pasting socketid in room section to send private message to a users by another.

--- be/server.js
+++ be/server.js
@@ -8,8 +8,15 @@ const io = new Server(3000, {
 io.on("connection", (socket) => {
 	console.log(socket.id);
 
-	socket.on("send-message", (message) => {
-		console.log(message);
-		io.emit("receive-message", message);
+	socket.on("send-message", (message, room) => {
+		console.log(message, "before room test", room);
+		if (room) {
+			console.log(message, "in room");
+			socket.to(room).emit("receive-message", message);
+			return;
+		} else {
+			console.log(message, "in broadcast");
+			socket.broadcast.emit("receive-message", message);
+		}
 	});
 });
